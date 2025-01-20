a8b6af0148315d9746b47992bfab4d578238bcc1
meenshery <160030641+meenshery@users.noreply.github.com>
2025-01-20 12:02:19 +0300

    Add files via upload

---
+++ домашка 1/javascript.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // задание 1
+    let str 
+    str = 'abcde'
+
+    alert (str[0])
+    alert (str[1])
+    alert (str[2])
+    alert (str[3])
+    alert (str[4])
+
+    // задание 2
+
+    let a = prompt("первое число")
+    let b = prompt("второе чисто")
+    console.log (a>0 || b>0)
+
+    // задание 3
+
+    let age = prompt('сколько вам лет??')
+    if (age >= 18) {
+        console.log ('совершеннолетний')}
+    else {
+        console.log ('несовершеннолетний')}
+ })
