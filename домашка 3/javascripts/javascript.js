accc45299391937f5397c178f654c9fa1d5626e1
meenshery <160030641+meenshery@users.noreply.github.com>
2025-02-16 20:36:48 +0300

    Add files via upload

---
+++ домашка 3/javascripts/javascript.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let box = document.querySelectorAll(".clickbox");
+    box.forEach(function(clickbox){
+        clickbox.addEventListener("click", function(){
+            if(clickbox.style.backgroundColor === "lightsteelblue"){
+                clickbox.style.backgroundColor = "yellow";
+                clickbox.style.margin = '2vw';
+                console.log("change color");
+            }
+            else{
+                clickbox.style.backgroundColor = "lightsteelblue";
+                clickbox.style.margin = '1vw';
+            }
+        });
+    });
+    });
+
